123f0c97367841c228759671039687960d5febc6
Lrunlin <liurl0621@gmail.com>
2025-02-27 12:59:44 +0800

    签名函数修改

--- sdk/src/mac/index.ts
+++ sdk/src/mac/index.ts
@@ -1,7 +1,7 @@
-import sha1 from "sha1";
-import { v4 } from "uuid";
+import sha from "sha256";
 
 /**
+ * @description 接口签名 传递Token 返回值添加到请求头
  * @param AccessKeyID {string} 配置AccessKeyID
  * @param AccessKeySecret {string} 配置AccessKeySecret
  * @returns x-AccessKeyID {string} AccessKeyID-添加至请求头
@@ -10,10 +10,10 @@ import { v4 } from "uuid";
  * @returns x-nonce {string} 随机数-添加至请求头
  */
 function mac(AccessKeyID: string, AccessKeySecret: string) {
-  let option = { ["x-data"]: +new Date(), ["x-nonce"]: v4() };
+  let option = { ["x-data"]: +new Date(), ["x-nonce"]: Math.random() };
   return {
     ["x-AccessKeyID"]: AccessKeyID,
-    ["x-Signature"]: sha1(option["x-data"] + option["x-nonce"] + AccessKeyID + AccessKeySecret),
+    ["x-Signature"]: sha(option["x-data"] + option["x-nonce"] + AccessKeyID + AccessKeySecret),
     ...option,
   };
 }
