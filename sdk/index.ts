5f52390f06490479fd5e4e0f303dd747efa3d19e
Lrunlin <liurl0621@gmail.com>
2025-02-27 13:54:41 +0800

    🐞 fix:

--- sdk/index.ts
+++
@@ -1,6 +0,0 @@
-import "./src/index";
-import * as tool from "./src/index";
-import { uuid, id, getAllRouter, mysql } from "./src/index";
-
-export default tool;
-export { uuid, id, getAllRouter, mysql };
